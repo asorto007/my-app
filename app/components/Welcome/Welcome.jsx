eb40a6c6de1a151e06a8368f0dd02ffeea78f22c
Anthony Sorto <asorto3@uic.edu>
2023-10-31 12:00:11 -0500

    added jessicas UI to our project

---
+++ app/components/Welcome/Welcome.jsx
@@ -0,0 +1,19 @@
+// Welcome.jsx
+"use client";
+import React from "react";
+import "./welcome.css";
+/* eslint-disable react/prop-types */
+
+function Welcome({ onDiaryClick }) {
+  return (
+    <div>
+      <h1>GitWell</h1>
+      <p>Take care of your mental health right now</p>
+      <button className="diary-button" onClick={onDiaryClick}>
+        📓 My diary
+      </button>
+    </div>
+  );
+}
+
+export default Welcome;
