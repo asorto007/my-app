9d66923e3799128d3899a2c883e98d99e2c1a7c4
Srivardhan Karnati <srivar.karnati@gmail.com>
2023-11-01 13:52:28 -0500

    added description of our web app

--- app/components/Welcome/Welcome.jsx
+++ app/components/Welcome/Welcome.jsx
@@ -9,6 +9,12 @@ function Welcome({ onDiaryClick }) {
     <div>
       <h1>GitWell</h1>
       <p>Take care of your mental health right now</p>
+      <p>Description: Our app currently has a welcome screen as well as a place where
+         the user can store their feelings (still needs to be implemented). We also
+         have ChatGPT integrated so that users will be able to document their emotions, experience, 
+         and feelings, in which that will help the AI to provide feedback towards that user. To
+         interact with ChatGPT, go to: <a href="https://my-app-rust-ten.vercel.app/dashboard" style={{ color: 'blue' }}>Dashboard</a>
+      </p>
       <button className="diary-button" onClick={onDiaryClick}>
         📓 My diary
       </button>
