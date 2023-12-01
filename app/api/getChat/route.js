e763013bc33f2657bf28f357ad97d4b3c29828c3
sri-karnati <57418863+sri-karnati@users.noreply.github.com>
2023-12-01 15:47:18 -0600

    Update route.js to test with original
    took out added stuff and stuck with default

--- app/api/getChat/route.js
+++ app/api/getChat/route.js
@@ -22,73 +22,73 @@ const openaiInstance = new openai.OpenAI({
 
 // export const runtime = "edge";
 
-const fetchWithRetry = async (fetchFunction, maxRetries = 3, delay = 1000) => {
-  let retries = maxRetries;
+// const fetchWithRetry = async (fetchFunction, maxRetries = 3, delay = 1000) => {
+//   let retries = maxRetries;
 
-  while (retries > 0) {
-    try {
-      const response = await fetchFunction();
-      return response; // Return on successful response
-    } catch (error) {
-      retries--;
-      if (retries === 0) throw error; // Rethrow error if no retries left
-      await new Promise((resolve) => setTimeout(resolve, delay)); // Wait for specified delay
-    }
-  }
-};
-
-export async function POST(req, res) {
-  try {
-    const userInput = await req.json();
-    console.log(`Received user input: ${userInput}`);
-
-    const fetchOpenAI = async () => {
-      return await openaiInstance.chat.completions.create({
-        messages: [
-          {
-            role: "system",
-            content: "You are a helpful assistant for mental health.",
-          },
-          { role: "user", content: userInput },
-        ],
-        model: "gpt-3.5-turbo",
-      });
-    };
-
-    const response = await fetchWithRetry(fetchOpenAI);
-
-    const chatGptResponse = response.choices[0].message.content;
-    return Response.json({ text: chatGptResponse });
-  } catch (error) {
-    console.error("Error:", error);
-    return Response.error({ error: "Internal Server Error" });
-  }
-}
+//   while (retries > 0) {
+//     try {
+//       const response = await fetchFunction();
+//       return response; // Return on successful response
+//     } catch (error) {
+//       retries--;
+//       if (retries === 0) throw error; // Rethrow error if no retries left
+//       await new Promise((resolve) => setTimeout(resolve, delay)); // Wait for specified delay
+//     }
+//   }
+// };
 
 // export async function POST(req, res) {
 //   try {
 //     const userInput = await req.json();
-//     // const { userInput } = body;
-//     // console.log("req", req);
-//     // console.log("userInput", req.body);
 //     console.log(`Received user input: ${userInput}`);
 
-//     const response = await openaiInstance.chat.completions.create({
-//       messages: [
-//         {
-//           role: "system",
-//           content: "You are a helpful assistant for mental health.",
-//         },
-//         { role: "user", content: userInput },
-//       ],
-//       model: "gpt-3.5-turbo",
-//     });
+//     const fetchOpenAI = async () => {
+//       return await openaiInstance.chat.completions.create({
+//         messages: [
+//           {
+//             role: "system",
+//             content: "You are a helpful assistant for mental health.",
+//           },
+//           { role: "user", content: userInput },
+//         ],
+//         model: "gpt-3.5-turbo",
+//       });
+//     };
+
+//     const response = await fetchWithRetry(fetchOpenAI);
 
 //     const chatGptResponse = response.choices[0].message.content;
-
 //     return Response.json({ text: chatGptResponse });
 //   } catch (error) {
 //     console.error("Error:", error);
 //     return Response.error({ error: "Internal Server Error" });
 //   }
 // }
+
+export async function POST(req, res) {
+  try {
+    const userInput = await req.json();
+    // const { userInput } = body;
+    // console.log("req", req);
+    // console.log("userInput", req.body);
+    console.log(`Received user input: ${userInput}`);
+
+    const response = await openaiInstance.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: "You are a helpful assistant for mental health.",
+        },
+        { role: "user", content: userInput },
+      ],
+      model: "gpt-3.5-turbo",
+    });
+
+    const chatGptResponse = response.choices[0].message.content;
+
+    return Response.json({ text: chatGptResponse });
+  } catch (error) {
+    console.error("Error:", error);
+    return Response.error({ error: "Internal Server Error" });
+  }
+}
